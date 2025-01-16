c0ba75bc828dbd4adbcbb9e96f290da5908a2dd2
sarthakk <sarthak20.sonawane@gmail.com>
2025-01-16 11:14:35 +0530

    api response handlers for errors and for responses

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,11 @@
+// this is the standardize way of sending the response to the frontend or to the user
+
+class ApiResponse {
+  constructor(statusCode, data, message = "success") {
+    this.statusCode = statusCode;
+    this.data = data;
+    this.message = message;
+    this.success = statusCode < 400;
+  }
+}
+export { ApiResponse };
