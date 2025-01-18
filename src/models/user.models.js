3f27f50e45473f1f6ecec11eaface3b0b70888d4
sarthakk <sarthak20.sonawane@gmail.com>
2025-01-18 09:40:41 +0530

    added few DB models

---
+++ src/models/user.models.js
@@ -0,0 +1,51 @@
+import mongoose, { Schema } from "mongoose";
+
+const Userschema = new Schema(
+  {
+    // id is automatically added by the mongoDB everytime we make any schema or data
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      index: true,
+      lowercase: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //cloudinary url(string)
+      required: true,
+    },
+    coverImage: {
+      type: String, //cloudinary url(string)
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = mongoose.model("User", Userschema);
---
+++ src/models/video.models.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const Videoschema = new Schema(
+  {
+    videoFile: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, //cloudinary url
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Video = mongoose.model("Video", Videoschema);
