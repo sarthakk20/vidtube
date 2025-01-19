d5b334cf57536d6984a886a683597319f6cf5262
sarthakk <sarthak20.sonawane@gmail.com>
2025-01-20 01:32:13 +0530

    pre hook and jwt

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const Userschema = new Schema(
   {
@@ -47,5 +49,43 @@ const Userschema = new Schema(
   },
   { timestamps: true }
 );
+
+// Using the pre hooks to encryt the password before saving it
+
+Userschema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+
+  this.password = bcrypt.hash(this.password, 10);
+  next();
+});
+
+Userschema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+Userschema.methods.generateAccessToken = function () {
+  // ṣhort lived access token
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+
+Userschema.methods.generateRefreshToken = function () {
+  // ṣhort lived access token
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
 
 export const User = mongoose.model("User", Userschema);
