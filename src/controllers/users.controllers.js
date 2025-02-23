d5ea34ab1687f8f2b863f64dc02f3dc987629f40
sarthakk <sarthak20.sonawane@gmail.com>
2025-02-23 22:14:13 +0530

    loginUser - generated accessToken and refreshToken

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -6,7 +6,29 @@ import {
   uploadToCloudinary,
   deleteFromCloudinary,
 } from "../utils/cloudinary.js";
-import { log } from "console";
+
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+
+    // small check for user existence
+    if (!user) {
+      throw new ApiError(500, "User not found");
+    }
+
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { accessToken, refreshToken };
+  } catch (error) {
+    throw new ApiError(
+      500,
+      "Something went wrong while generating acces and refresh tokens..."
+    );
+  }
+};
 
 const registerUser = asyncHandler(async (req, res) => {
   // todo
@@ -90,9 +112,6 @@ const registerUser = asyncHandler(async (req, res) => {
     if (avatar) {
       await deleteFromCloudinary(avatar.public_id);
     }
-    if (avatar) {
-      await deleteFromCloudinary(avatar.public_id);
-    }
     if (coverImage) {
       await deleteFromCloudinary(coverImage.public_id);
     }
@@ -102,4 +121,53 @@ const registerUser = asyncHandler(async (req, res) => {
     );
   }
 });
-export { registerUser };
+
+const loginUser = asyncHandler(async (req, res) => {
+  // get data from the body
+  const { email, username, password } = req.body;
+
+  // validate
+  if ([email, username, password].some((fields) => fields?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
+
+  // find user into the DB, based on either username or email
+  const user = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!user) {
+    throw new ApiError(404, "User not found");
+  }
+  // validate password
+  const isPasswordValid = await isPasswordCorrect(password);
+
+  if (!isPasswordValid) {
+    throw new ApiError(401, "Invalid Credentials");
+  }
+
+  const { accessToken, refreshToken } = await generateAccessAndRefreshToken(
+    user._id
+  );
+
+  const loggedInUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!loggedInUser) {
+    throw new ApiError(403, "Error while logging in");
+  }
+
+  const options = {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === " production",
+  };
+
+  res
+    .statusCode(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(new ApiResponse(200, loggedInUser, "User logged in successfully"));
+});
+
+export { registerUser, loginUser };
